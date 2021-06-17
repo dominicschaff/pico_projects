f6ba84ce74bc981565611e835bb1040c1415a888
Dominic Schaff <dominic@schaff.dev>
2021-06-17 15:24:45 +0200

    Cleanup and allow partial refresh

--- weather/main.py
+++ weather/main.py
@@ -1,25 +1,18 @@
 from mylib import Weather, CreateI2C
 
 import utime
-from machine import Pin, I2C
 
 import ahtx0
 
-# cause a wait
-led = Pin(25, Pin.OUT)
-for i in range(10):
-    led.toggle()
-    utime.sleep(0.5)
-
-# I2C for the Wemos D1 Mini with ESP8266
 i2c = CreateI2C()
-weather = Weather()
 
-# Create the sensor object using I2C
+# 30(minutes), 60(seconds per minute), 2(refreshes twice a minute)
+weather = Weather(graph_every=30*60*2)
+
 sensor = ahtx0.AHT10(i2c)
 
 while True:
     weather.vals(sensor.temperature, sensor.relative_humidity, 0)
     weather.update()
-    utime.sleep(60)
+    utime.sleep(30)
 
--- weather/mylib.py
+++ weather/mylib.py
@@ -61,7 +61,7 @@ class Screen:
             py=e
 
 class Weather:
-    def __init__(self):
+    def __init__(self, graph_every):
         self.screen = Screen()
         self.temps = []
         self.humids = []
@@ -70,34 +70,48 @@ class Weather:
         self.humid = 0
         self.barom = 0
         self.last = 0
-    
+        self.graph_every = graph_every
+        self.full_refresh = True
+
     def update(self):
-        self.screen.clear()
-        self.screen.box(21, 21, 42)
-        self.screen.box(64, 21, 42)
-        self.screen.box(107, 21, 42)
+        if self.full_refresh:
+            self.screen.clear()
+        else:
+            self.screen.box_fill(21, 21, 40, 1)
+            self.screen.box_fill(64, 21, 40, 1)
+            self.screen.box_fill(107, 21, 40, 1)
+
         self.screen.text_center("%.1f" % self.temp, 21, 21)
         self.screen.text_center("%.0f%%" % self.humid, 64, 21)
         self.screen.text_center("%.0f" % self.barom, 107, 21)
-        
-        self.screen.graph("Temperature:", self.temps, 64, 100, 50)
-        self.screen.graph("Humidity:", self.humids, 64, 175, 50)
-        self.screen.graph("Barometer:", self.baroms, 64, 250, 50)
-        self.screen.show()
-    
+
+        if self.full_refresh:
+            self.screen.box(21, 21, 42)
+            self.screen.box(64, 21, 42)
+            self.screen.box(107, 21, 42)
+            self.screen.graph("Temperature:", self.temps, 64, 100, 50)
+            self.screen.graph("Humidity:", self.humids, 64, 175, 50)
+            self.screen.graph("Barometer:", self.baroms, 64, 250, 50)
+            self.screen.show()
+        else:
+            self.screen.show_partial()
+        self.full_refresh = False
+
     def vals(self, temp, humid, barom):
+        print("Set: temp:%.1f | humid:%.1f | barom:%.1f" % (temp, humid, barom))
         self.temp = temp
         self.humid = humid
         self.barom = barom
         self.last -= 1
         if self.last <= 1:
-            self.last = 30
+            self.full_refresh = True
+            self.last = self.graph_every
             self.temps.append(self.temp)
             self.humids.append(self.humid)
             self.baroms.append(self.barom)
-            while len(self.temps) > 100:
+            while len(self.temps) > 120:
                 self.temps.pop(0)
-            while len(self.humids) > 100:
+            while len(self.humids) > 120:
                 self.humids.pop(0)
-            while len(self.baroms) > 100:
+            while len(self.baroms) > 120:
                 self.baroms.pop(0)
